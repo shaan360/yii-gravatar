5b98bc543823e2c4426c9f3a658d287526624447
Sergey Malyshev <lunga.web@gmail.com>
2011-06-29 14:22:28 +0300

    Unnamed commit

---
+++ yii-gravatar/YiiGravatar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * YiiGravatar class file.
+ *
+ * @author Sergey Malyshev <malyshev.php@gmail.com>
+ */
+
+/**
+ * YiiGravatar represents an ...
+ *
+ * Description of YiiGravatar
+ *
+ * @author Sergey Malyshev <malyshev.php@gmail.com>
+ * @version $Id$
+ * @package
+ * @since 1.1.7
+ */
+class YiiGravatar extends CComponent
+{
+    
+}
---
+++ yii-gravatar/YiiGravatarWidget.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * YiiGravatarWidget class file.
+ *
+ * @author Sergey Malyshev <malyshev.php@gmail.com>
+ */
+
+/**
+ * YiiGravatarWidget represents an ...
+ *
+ * Description of YiiGravatarWidget
+ *
+ * @author Sergey Malyshev <malyshev.php@gmail.com>
+ * @version $Id$
+ * @package
+ * @since 1.1.7
+ */
+class YiiGravatarWidget extends CWidget
+{
+    
+}
